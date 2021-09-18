b3ef9083b99c419244ad173192db1b6dbc2f2854
ashissumandash <ashis0601223380@gmail.com>
2021-09-18 20:14:31 +0530

    commit multiply code

---
+++ DSA_Program/src/com/dsaprogram/ml/Multiply.java
@@ -0,0 +1,20 @@
+package com.dsaprogram.ml;
+import java.util.*
+
+public class Multiply {
+	
+	public static void main (String[] args){
+		
+		int a,b,c;
+		Scanner sc = new Scanner(System.in);
+		a=Integer.parseInt(sc.nextLine());
+		b=Integer.parseInt(sc.nextLine());
+		
+		
+		System.out.println("Multiple value is: "+(a*b));
+				
+		
+		
+	}
+
+}
