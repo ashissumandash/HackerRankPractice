fe8a2e32ddeb874bfc682d6355bd27edd637ff51
ashissumandash <ashis0601223380@gmail.com>
2021-09-18 18:56:01 +0530

    Initial commit for my first program

---
+++ DSA_Program/src/com/dsaprogram/ml/Streaming.java
@@ -0,0 +1,28 @@
+package com.dsaprogram.ml;
+
+import java.util.*;
+
+
+public class Streaming{
+    public static void main(String[] args){
+        int firstNum,secondNum,thirdNum;
+        
+        Scanner sc = new Scanner (System.in);
+     //   System.out.println("Enter first number:");
+        firstNum = Integer.parseInt(sc.nextLine());
+     //   System.out.println("Enter second number:");
+        secondNum = Integer.parseInt(sc.nextLine());
+     //   System.out.println("Enter third number:");
+        thirdNum= Integer.parseInt(sc.nextLine());
+        
+     //   System.out.println("First number is:");
+        System.out.println(firstNum);
+     //   System.out.println("Secind number is:");
+        System.out.println(secondNum);
+     //   System.out.println("Third number is:");
+        System.out.println(thirdNum);
+        
+        
+    }
+    
+}
